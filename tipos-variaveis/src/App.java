259a4821783bcf5b1414c125d770a76a5647b355
Vanderlei Teixeira <vanderleimasp@yahoo.com.br>
2024-06-13 15:28:39 -0300

    aula de operadores

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    }
+}
